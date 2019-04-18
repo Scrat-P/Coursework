7929203a6410ca11cab71055974401de5609586e
Andrey_Yurevich <and9019468@yandex.ru>
2019-04-18 19:20:03 +0300

    Moved functionality of transferring images to classes

--- client/sender.py
+++ client/sender.py
@@ -1,42 +1,30 @@
 import socket
 
+TCP_IP = "127.0.0.1"
+TCP_PORT = 5001
 
-def get_img_1():
-    from PIL import Image
+class Sender:
+    def __init__(self):
+        self.tcp_ip = TCP_IP
+        self.tcp_port = TCP_PORT
 
-    roiImg = Image.new("RGB", [5, 5], "red")
-
-    image = roiImg.tobytes()
-
-    return image
-
-
-def get_img_2():
-    from PIL import Image
-
-    roiImg = Image.new("RGB", [5, 5], "blue")
-
-    image = roiImg.tobytes()
-
-    return image
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.tcp_ip, self.tcp_port))
 
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5001
+    def convert_to_bytes(self, img):
+        return img.tobytes()
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((UDP_IP, UDP_PORT))
-size = 1024
 
-def test(img):
-    for i in range(round(len(img) / 64) + 2):
-        part_img = img[i * 64:(i + 1) * 64]
-        client_socket.sendto(part_img, (UDP_IP, UDP_PORT))
-        if not part_img:
-            client_socket.sendto(bytearray("#5:5", "utf-8"), (UDP_IP, UDP_PORT))
-            client_socket.recvfrom(1000)
-            break
-        client_socket.recvfrom(1000)
+    def send_image(self, canvas, width, height):
+        img = self.convert_to_bytes(canvas)
 
-test(get_img_1())
-test(get_img_2())
+        for i in range(round(len(img) / 64) + 2):
+            part_img = img[i * 64:(i + 1) * 64]
+            self.client_socket.sendto(part_img, (self.tcp_ip, self.tcp_port))
+            if not part_img:
+                self.client_socket.sendto(bytearray(f"#{width}:{height}", "utf-8"), (self.tcp_ip, self.tcp_port))
+                self.client_socket.recvfrom(1000)
+
+                break
+            self.client_socket.recvfrom(1000)
--- server/recipient.py
+++ server/recipient.py
@@ -1,38 +1,46 @@
 import socket
 
 
-def call_save_as_image(fname, img, width, height):
-    from PIL import Image
+TCP_IP = "127.0.0.1"
+TCP_PORT = 5001
 
-    image = Image.frombytes("RGB", (width, height), img)
-    with open(fname + ".png", "wb") as file:
-        image.save(file)
+class Recipient:
+    def __init__(self):
+        self.tcp_ip = TCP_IP
+        self.tcp_port = TCP_PORT
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5001
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.connect((self.tcp_ip, self.tcp_port))
+        self.server_socket.listen(1)
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((UDP_IP, UDP_PORT))
-server_socket.listen(1)
+        self.client_socket, _ = self.server_socket.accept()
 
-client_socket, address = server_socket.accept()
+    def call_save_as_image(self, fname, img, width, height):
+        from PIL import Image
 
-while True:
-    fname = "picture"
-    img = b''
-    while True:
-        temp = client_socket.recvfrom(512)
-        client_socket.sendto(bytearray("message", "utf-8"), (UDP_IP, UDP_PORT))
-        if temp[0] and temp[0][0] == 35:
-            print(len(img))
-            size = temp[0][1:]
-            width, height = str(size, "utf-8").split(":")
-            width, height = int(width), int(height)
-            print(width, height)
-            call_save_as_image(fname, img, width, height)
+        image = Image.frombytes("RGB", (width, height), img)
+        with open(fname + ".png", "wb") as file:
+            image.save(file)
+
+    def receive_images(self):
+        while True:
+            fname = "picture"
             img = b''
-            fname = "1"
-        else:
-            img += temp[0]
-        
-    exit()
+            while True:
+                temp = self.client_socket.recvfrom(512)
+                self.client_socket.sendto(bytearray("message", "utf-8"), (self.tcp_ip, self.tcp_port))
+                if temp[0] and temp[0][0] == 35:
+                    size = temp[0][1:]
+
+                    width, height = str(size, "utf-8").split(":")
+                    width, height = int(width), int(height)
+
+                    self.call_save_as_image(fname, img, width, height)
+                    img = b''
+                    fname = "1"
+                else:
+                    img += temp[0]
+
+if __name__ == "__main__":
+    n = Recipient()
+    n.receive_images()
