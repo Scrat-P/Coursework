a8b86c73d9751c5fdc3aa3cf9ad715365f83bcc1
Andrey_Yurevich <and9019468@yandex.ru>
2019-04-18 18:50:42 +0300

    Add transferring multiple images to server

--- client/sender.py
+++ client/sender.py
@@ -1,10 +1,20 @@
 import socket
 
 
-def get_img():
+def get_img_1():
     from PIL import Image
 
-    roiImg = Image.new("RGB", [10, 10], "red")
+    roiImg = Image.new("RGB", [5, 5], "red")
+
+    image = roiImg.tobytes()
+
+    return image
+
+
+def get_img_2():
+    from PIL import Image
+
+    roiImg = Image.new("RGB", [5, 5], "blue")
 
     image = roiImg.tobytes()
 
@@ -12,24 +22,21 @@ def get_img():
 
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5006
+UDP_PORT = 5001
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((UDP_IP, UDP_PORT))
 size = 1024
 
-while True:
-    temp = client_socket.recvfrom(1024)
-    data = temp[0].decode("utf-8")
-    print("The following data was received - ", data)
-    print("Opening file - ", data)
-    img = get_img()
-
-    for i in range(round(len(img) / 64)):
+def test(img):
+    for i in range(round(len(img) / 64) + 2):
         part_img = img[i * 64:(i + 1) * 64]
-        print(i)
-        if not img:
+        client_socket.sendto(part_img, (UDP_IP, UDP_PORT))
+        if not part_img:
+            client_socket.sendto(bytearray("#5:5", "utf-8"), (UDP_IP, UDP_PORT))
+            client_socket.recvfrom(1000)
             break
-        client_socket.sendto(img, (UDP_IP, UDP_PORT))
-    print("Data sent successfully")
-    exit()
+        client_socket.recvfrom(1000)
+
+test(get_img_1())
+test(get_img_2())
--- server/recipient.py
+++ server/recipient.py
@@ -1,15 +1,15 @@
 import socket
 
 
-def call_save_as_image(fname, img):
+def call_save_as_image(fname, img, width, height):
     from PIL import Image
 
-    image = Image.frombytes("RGB", (10, 10), img)
+    image = Image.frombytes("RGB", (width, height), img)
     with open(fname + ".png", "wb") as file:
         image.save(file)
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5006
+UDP_PORT = 5001
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((UDP_IP, UDP_PORT))
@@ -19,16 +19,20 @@ client_socket, address = server_socket.accept()
 
 while True:
     fname = "picture"
-    client_socket.sendto(bytearray(fname, "utf-8"), (UDP_IP, UDP_PORT))
     img = b''
     while True:
         temp = client_socket.recvfrom(512)
-        print(temp)
-        if not temp[0]:
-            break
-
-        img += temp[0]
+        client_socket.sendto(bytearray("message", "utf-8"), (UDP_IP, UDP_PORT))
+        if temp[0] and temp[0][0] == 35:
+            print(len(img))
+            size = temp[0][1:]
+            width, height = str(size, "utf-8").split(":")
+            width, height = int(width), int(height)
+            print(width, height)
+            call_save_as_image(fname, img, width, height)
+            img = b''
+            fname = "1"
+        else:
+            img += temp[0]
         
-    call_save_as_image(fname, img)
-    print("Data Received successfully")
     exit()
